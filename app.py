d1bdabd5f5a10e7cff30e3cd1446a49e1ea4c7b1
Amzar123 <aji.muhammad.zapar080500@gmail.com>
2023-11-11 16:48:16 +0700

    temp

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask
-# from flask_script import Manager
-# from flask_migrate import Migrate, MigrateCommand
 from src.utils.db import db
 # Import controllers
 from src.controllers.recommendation import Recommendation
@@ -12,7 +10,7 @@ app.config.from_object('src.utils.setting.Config')
 db.init_app(app)
 
 # adding routes
-app.add_url_rule('/', view_func=Recommendation.HelloWord, methods=['GET',])
+app.add_url_rule('/recommendation', view_func=Recommendation.HelloWord, methods=['GET',])
 
 #run
 if __name__ == '__main__':
--- src/controllers/recommendation.py
+++ src/controllers/recommendation.py
@@ -3,8 +3,71 @@ from src.models.test_result import TestResult;
 from src.models.test_result_detail import TestResultDetail;
 from src.models.user import User;
 
+# import render template
+from flask import render_template
+
 class Recommendation():
     def __init__(self, user):
         self.user = user
     def HelloWord():
         return "hello word"
+    
+    # get all question bank 
+    @staticmethod
+    def recommendation(self):
+        # get user data from db 
+        question_banks = QuestionBank.getAll()
+        
+        # create test result and detail for each question bank
+        for question_bank in question_banks:
+            test_result = TestResult.create(question_bank.id, self.user.id)
+            test_result_details = TestResultDetail.create(test_result.id, question_bank.id)
+            # calculate score
+            total_score = 0
+            correct_answer_count = 0
+            for test_result_detail in test_result_details:
+                total_score += test_result_detail.score
+                if test_result_detail.score == 1:
+                    correct_answer_count += 1
+            # update score for test result
+            test_result.update(total_score, correct_answer_count)
+        # get test result
+        test_results = TestResult.getByUserId(self.user.id)
+        # get test result detail
+        test_result_details = TestResultDetail.getByUserId(self.user.id)
+        # get user
+        user = User.getById(self.user.id)
+        # calculate score
+        total_score = 0
+        correct_answer_count = 0
+        for test_result_detail in test_result_details:
+            total_score += test_result_detail.score
+            if test_result_detail.score == 1:
+                correct_answer_count += 1
+        # update score for user
+        user.update(total_score, correct_answer_count)
+        # get all user
+        users = User.getAll()
+        # sort user by score
+        users.sort(key=lambda x: x.total_score, reverse=True)
+        # get user rank
+        user_rank = 0
+        for index, user in enumerate(users):
+            if user.id == self.user.id:
+                user_rank = index + 1
+                break
+        # get user rank percentage
+        user_rank_percentage = 0
+        if user_rank > 0:
+            user_rank_percentage = (user_rank / len(users)) * 100
+        # get user rank percentage
+
+        # get user rank percentage
+        user_rank_percentage = 0
+        if user_rank > 0:
+            user_rank_percentage = (user_rank / len(users)) * 100
+        # get user rank percentage
+        return render_template('dashboard/index.html', title='Dashboard', user=user, test_results=test_results
+            , test_result_details=test_result_details, users=users, user_rank=user_rank
+            , user_rank_percentage=user_rank_percentage)
+    
