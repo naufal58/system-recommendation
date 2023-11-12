36332ab71a628340a2962517c24e11d01bfbfc92
Melvyn <melvyndede@gmail.com>
2023-11-12 23:57:26 +0700

    implement gunicorn

---
+++ src/utils/gunicorn.conf.py
@@ -0,0 +1,12 @@
+# Gunicorn config file
+
+# Number of worker processes to run
+workers = 2
+
+# Host and port to bind to
+bind = "0.0.0.0:5006"
+
+threads = 8
+
+# Timeout for worker processes
+timeout = 0
