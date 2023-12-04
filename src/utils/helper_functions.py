769f5861a1bf18e55ce29660c7184a18156b0cc5
Melvyn <melvyndede@gmail.com>
2023-12-05 05:10:08 +0700

    refactor

--- src/utils/helper_functions.py
+++ src/utils/helper_functions.py
@@ -1,6 +1,5 @@
 import json
 import os
-from src.pipeline.extract_features import extract_features
 import random
 import string
  
@@ -10,9 +9,9 @@ def random_string(n):
  
     return res
 
-def get_training_data():
-    path = os.getcwd()
-    data_path = path + '/data/training_data.json'
+def get_training_data(filename = "training_data"):
+    path = os.path.join(os.getcwd(), 'data')
+    data_path = os.path.join(path, filename + '.json')
 
     with open(data_path, 'r') as training_file:
         training_data = json.load(training_file)
@@ -35,22 +34,3 @@ def preprocess_underlined(underline):
     for i in underline.split(','):
         underline_list.append(int(i.split('-')[0]))
     return underline_list
-
-def demo_extract(count=1, shuffle=False):
-    training_data = get_training_data()
-    new_training_data = []
-    counter = 0
-    if shuffle == True:
-        random.shuffle(training_data['data'])
-    for data in training_data['data']:
-        demo_pipeline = extract_features(data['text'], preprocess_underlined(data['underline']))
-        new_training_data.append(demo_pipeline)
-        counter += 1
-        print(f'{counter} questions extracted.')
-        if counter == count:
-            break
-    if set_training_data({'data': new_training_data}):
-        return True
-    else:
-        return False
-
