a299f9fc79c0b7c355fc7e717a86d7d020bc9b28
Melvyn <melvyndede@gmail.com>
2023-12-06 23:04:30 +0700

    add essential_features_only param

--- src/pipeline/extract_features.py
+++ src/pipeline/extract_features.py
@@ -1,16 +1,18 @@
 from src.controllers.feature_extraction import FeatureExtraction
 from src.utils.helper_functions import get_training_data, preprocess_underlined, set_training_data
 
-def extract_features(text, underline):
+def extract_features(text, underline, essential_features_only=False):
     question_characteristics = FeatureExtraction(text, underline)
     response = {}
-    response['question_text'] = text
+    print('Getting tags features...')
+    if essential_features_only == False:
+        response['question_text'] = text
+        response['underlined_postags'], response['irregular_verbs'], response['regular_verbs'] = question_characteristics.tag_features()
+    else:
+        _, response['irregular_verbs'], response['regular_verbs'] = question_characteristics.tag_features()
 
     print('Getting tense type...')
     response['tense_type'] = question_characteristics.tenses_type()
-
-    print('Getting tags features...')
-    response['underlined_postags'], response['irregular_verbs'], response['regular_verbs'] = question_characteristics.tag_features()
 
     print('Counting homophones...')
     response['homophones'] = question_characteristics.count_homophones()
@@ -30,7 +32,7 @@ def extract_from_file(filename):
     training_data = get_training_data(filename)
     data_extract = []
     for data in training_data:
-        data_extract.append(extract_features(data['soal'], preprocess_underlined(data['underline'])))
+        data_extract.append(extract_features(data['soal'], preprocess_underlined(data['underline']), essential_features_only=True))
     set_training_data({'data': data_extract})
 
     return {'msg': f'Data questions from {filename} file has been extracted.'}
