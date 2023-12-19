57c0d661c9d315fa5a10acd9ee50099dd61c5e80
naufal58 <61070247+naufal58@users.noreply.github.com>
2023-12-19 12:20:01 +0700

    update recommendation sentence

--- src/controllers/recommendation.py
+++ src/controllers/recommendation.py
@@ -14,7 +14,7 @@ class Recommendation():
 
         df_discretized = pd.DataFrame()
 
-        not_included = ['tense_type', 'key_is_homophone', 'answer_is_homophone', 'error_type']
+        not_included = ['tense_type', 'key_is_homophone', 'answer_is_homophone', 'error_type', 'subject_verb_agreement']
         for column in df.columns:
             if column in not_included:
                 df_discretized[column] = df[column]
@@ -36,12 +36,13 @@ class Recommendation():
         encoded_data = encoder.fit(data_list).transform(data_list)
 
         df = pd.DataFrame(encoded_data, columns=encoder.columns_)
+        print (df)
 
         frequent_itemsets = fpgrowth(df, min_support=0.3, use_colnames=True)
 
         rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=0.7)
         
-        rules = rules.sort_values(by=['support', 'lift'], ascending=[False, False])
+        rules = rules.sort_values(by=['support', 'confidence', 'lift'], ascending=[False, False, False])
 
 
         rules = rules[rules.apply(lambda row: len(row['antecedents']) + len(row['consequents']) <= 3, axis=1)]
@@ -50,24 +51,8 @@ class Recommendation():
         
         return filtered_rules
 
-    def generate_recommendation(self, filtered_rules):
-        rules_list = filtered_rules.to_dict('records')
-        print(rules_list)
-        recommendation_sentence = []
-        for rule in rules_list:
-            antecedents = ', '.join(rule['antecedents'])
-            consequents = ', '.join(rule['consequents'])
-
-            action, slug = self.mapping_action(antecedents, consequents)
-
-            recommendation = f"You must {action}. You can learn on our website at smartengtest/{slug}."
-            recommendation_sentence.append(recommendation)
-        print(recommendation_sentence)
-            
-        return recommendation_sentence
-    
-    def mapping_action (self, antecedent,consequent):
-        actions = {
+    def get_actions(self):
+        return {
             'tenses types': ("learn more about modifier tenses", "past-tense"),
             'past': ("focus more on past tense usage including all the modifiers like past continuous, past future, and past present", "past-tense"),
             'present': ("concentrate on present tense structures", "present-tense"),
@@ -75,30 +60,26 @@ class Recommendation():
             'l_vocabulary_difficulty_score': ("enhance basic vocabulary skills", "vocabulary"),
             'm_vocabulary_difficulty_score': ("build on intermediate vocabulary", "vocabulary"),
             'h_vocabulary_difficulty_score': ("master advanced vocabulary", "vocabulary"),
-            'l_conjuctions': ("practice basic conjunctions", "conjunctions"),
-            'm_conjuctions': ("improve use of intermediate conjunctions", "conjunctions"),
-            'h_conjuctions': ("refine usage of complex conjunctions", "conjunctions"),
+            'l_conjunctions': ("practice basic conjunctions", "conjunctions"),
+            'm_conjunctions': ("improve use of intermediate conjunctions", "conjunctions"),
+            'h_conjunctions': ("refine usage of complex conjunctions", "conjunctions"),
             'l_flesch_reading_ease': ("read simpler texts to improve comprehension", "reading-materials"),
             'm_flesch_reading_ease': ("engage with moderately complex texts", "reading-materials"),
             'h_flesch_reading_ease': ("challenge yourself with complex reading materials", "reading-materials")
         }
-    
-        "You must focus more on past tense usage including all the modifier like past continous, past future, and past present. You can learn on our website at smartengtest/past-tense."
-        "You must focus more on past tense usage including all the modifier like past continous, past future, and past present. You can learn on our website at smartengtest/past-tenses."
-        paired_actions = {
-            ('past', 'tenses types'): "focus more on past tense usage including all the modifier like past continous, past future, and past present",
-            ('tenses types', 'past'): "focus more on past tense usage including all the modifier like past continous, past future, and past present",
-        }    
 
-        if (antecedent, consequent) in paired_actions:
-            action = paired_actions[(antecedent, consequent)]
-            # Assuming the consequent is a key in the actions dictionary for the URL slug
-            # If not, add a default slug here
-            slug = actions.get(consequent, ("past-tenses", ""))[1]
-        elif antecedent in actions:
-            action, slug = actions[antecedent]
-        else:
-            # Default action and slug if no match is found
-            action, slug = "perform the relevant activity", "general"
+    def generate_recommendation(self, filtered_rules):
+        actions = self.get_actions()
+        recommendations = []
 
-        return action, slug
+        for index, rule in filtered_rules.iterrows():
+            antecedents = ', '.join(list(rule['antecedents']))
+            consequents = ', '.join(list(rule['consequents']))
+
+            antecedent_action, antecedent_slug = actions.get(antecedents, ("", ""))
+            consequent_action, consequent_slug = actions.get(consequents, ("", ""))
+
+            recommendation = f"To improve in '{antecedents}', you should {antecedent_action}. Additionally, to enhance your skills in '{consequents}', {consequent_action}. Learn more at smartengtest/{antecedent_slug} and smartengtest/{consequent_slug}."
+            recommendations.append(recommendation)
+        
+        return recommendations
